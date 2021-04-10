e2f576dbab19af08efd4a78a0640d1333a9fbc1a
Shohruh <shokhrukh.abduahadov@gmail.com>
2021-04-10 21:31:06 +0500

    autpush

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-export DOMAIN=localhost
-export STACK_NAME=safed
-export TRAEFIK_PUBLIC_NETWORK=traefik-public
-export TRAEFIK_TAG=safed.ml
-export TRAEFIK_PUBLIC_TAG=traefik-public
-export DEFAULT_NETWORK=elastic-default-network
-export DEFAULT_NETWORK_TYPE=bridge
-
-export ELASTICSEARCH_USERNAME=elastic
-export ELASTICSEARCH_PASSWORD=changeme
-export ELASTIC_VERSION=6.6.0
-export DOMAIN=monitor.safed.ml
 # docker network rm elastic
 # docker network create -d overlay elastic
+
+# envsubst < logstash.conf.default > logstash.conf
+# docker stack deploy -c docker-compose.yml elastic
+
+
+
+
+
+# sudo docker stack deploy -c docker-stack.yml --with-registry-auth myid-$ENVIRONMENT
+
+
+# ------------
+
+set -e
+
+
 envsubst < logstash.conf.default > logstash.conf
-docker stack deploy -c docker-compose.yml elastic
+docker-compose --env-file .env -f docker-compose.yml config > docker-stack.yml;
+docker stack deploy -c docker-stack.yml elastic
