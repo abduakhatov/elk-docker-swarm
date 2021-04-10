aa3875ef780406b16d2df8bef5fb8a66e3889eb9
Shohruh <shokhrukh.abduahadov@gmail.com>
2021-04-10 13:33:40 +0500

    autpush

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ export ELASTICSEARCH_USERNAME=elastic
 export ELASTICSEARCH_PASSWORD=changeme
 export ELASTIC_VERSION=6.6.0
 export DOMAIN=monitor.safed.ml
-docker network rm elastic
-docker network create -d overlay elastic
+# docker network rm elastic
+# docker network create -d overlay elastic
 envsubst < logstash.conf.default > logstash.conf
 docker stack deploy -c docker-compose.yml elastic
