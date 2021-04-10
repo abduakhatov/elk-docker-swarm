aff6bf67872ab877cd4856f1bdae15939c737b2c
Shohruh <shokhrukh.abduahadov@gmail.com>
2021-04-10 13:27:13 +0500

    autpush

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-export TRAEFIK_PUBLIC_TAG=traefik-public
 export TRAEFIK_TAG=monitor.safed.ml
 export STACK_NAME=safed
+export DOMAIN=localhost
+export TRAEFIK_PUBLIC_NETWORK=traefik-public
+export TRAEFIK_PUBLIC_TAG=traefik-public
+export DEFAULT_NETWORK=elastic-network
+export DEFAULT_NETWORK_TYPE=bridge
+
+
+
+
 export ELASTICSEARCH_USERNAME=elastic
 export ELASTICSEARCH_PASSWORD=changeme
 export ELASTIC_VERSION=6.6.0
