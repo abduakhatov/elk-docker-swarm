e7663a15c9a9718d293fabbee1a45cb231a677ac
Shohruh <shokhrukh.abduahadov@gmail.com>
2021-06-29 15:54:36 +0500

    reset

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-# docker network rm elastic
-# docker network create -d overlay elastic
+# recreate network
+docker network rm elastic
+docker network create -d overlay elastic
 
-# envsubst < logstash.conf.default > logstash.conf
-# docker stack deploy -c docker-compose.yml elastic
+# substitude env empty env var names
+envsubst < logstash.conf.default > logstash.conf
 
-
-
-
-
-# sudo docker stack deploy -c docker-stack.yml --with-registry-auth myid-$ENVIRONMENT
-
-
-# ------------
-
-set -e
-
-
-# envsubst < logstash.conf.default > logstash.conf
+# deploy
 docker-compose --env-file .env -f docker-compose.yml config > docker-stack.yml;
 docker stack deploy -c docker-stack.yml elastic
