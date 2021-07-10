a5ac7a690eca13b35003ade2642b290615d0edc9
Shohruh <shokhrukh.abduahadov@gmail.com>
2021-07-10 15:49:12 +0500

    filebeat first config

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,11 @@ docker network create -d overlay elastic
 
 # substitude env empty env var names
 export $(cat .env)
+
+FILEBEAT_MONIT=filebeat/monitor
+
 envsubst < logstash.conf.default > logstash.conf
+envsubst < $FILEBEAT_MONIT/filebeat.yml.default > $FILEBEAT_MONIT/filebeat.yml
 
 # deploy
 docker-compose --env-file .env -f docker-compose.yml config > docker-stack.yml;
