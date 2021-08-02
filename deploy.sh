a10cd9f52911346d6be832c59a7b65bc1b479c18
Shohruh Abduahadov <shokhrukh.abduahadov@gmail.com>
2021-08-02 19:44:25 +0500

    autpush

--- deploy.sh
+++ deploy.sh
@@ -15,4 +15,5 @@ sh ./scripts/build-push.sh
 
 # deploy
 docker-compose --env-file .env -f docker-compose.yml config > docker-stack.yml;
-docker stack deploy -c docker-stack.yml elastic
+docker-compose --env-file .env -f docker-compose.tools.yml config > docker-stack.tools.yml;
+docker stack deploy -c docker-stack.yml -c docker-stack.tools.yml elastic
